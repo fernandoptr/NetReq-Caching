ea5695968b3681cc82f175abc0a276ff5b5d2718
Fernando Putra <fernnandoptr@gmail.com>
2024-01-07 07:43:53 +0700

    Initial Commit

---
+++ NetReq Caching/NetReq_CachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetReq_CachingApp.swift
+//  NetReq Caching
+//
+//  Created by Fernando Putra on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NetReq_CachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
